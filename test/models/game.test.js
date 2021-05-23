3547277ec089b3c51220538808a74ca6486267c8
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-05-23 21:08:25 +0100

    Finished initial test of game class

--- test/models/game.test.js
+++ test/models/game.test.js
@@ -2,6 +2,7 @@ const Game = require('../../src/models/game.js');
 const Controller = require('../../src/lib/controller');
 const Reporters = require('../../src/reporters.js');
 const Clients = require('../../src/clients.js');
+const Piece = require('../../src/models/piece.js');
 
 function new_game() {
   return new Game(
@@ -96,10 +97,10 @@ test('should run a clients turn method', () => {
   var game = new_game();
   var dwarf_turn = jest.fn();
   var dwarf_end_turn = jest.fn();
-  game.clients.d = {turn: dwarf_turn, end_turn: dwarf_end_turn,controller: game.dwarf_controller};
+  game.clients.d = {turn: dwarf_turn, end_turn: dwarf_end_turn, controller: game.dwarf_controller};
   var troll_turn = jest.fn();
   var troll_end_turn = jest.fn();
-  game.clients.t = {turn: troll_turn, end_turn: troll_end_turn,controller: game.troll_controller};
+  game.clients.t = {turn: troll_turn, end_turn: troll_end_turn, controller: game.troll_controller};
 
   expect(game.current_side).toEqual('d');
   expect(dwarf_turn).not.toHaveBeenCalled();
@@ -122,5 +123,92 @@ test('should run a clients turn method', () => {
   expect(game.current_side).toEqual('d');
   expect(troll_end_turn).toHaveBeenCalled();
   expect(dwarf_turn).toHaveBeenCalledTimes(2);
+});
 
+test('gets current client', () => {
+  var game = new_game();
+  expect(game.current_side).toEqual('d');
+  expect(game.current_client()).toEqual(game.clients.d);
+  expect(game.current_controller()).toEqual(game.dwarf_controller);
+  game.dwarf_controller.select_space(4, 1);
+  game.dwarf_controller.move(8, 5);
+  expect(game.current_side).toEqual('t');
+  expect(game.current_client()).toEqual(game.clients.t);
+  expect(game.current_controller()).toEqual(game.troll_controller);
+});
+
+test('moves a piece', () => {
+  var game = new_game();
+  game.dwarf_controller.select_space(4, 1);
+  game.move_piece(8, 5, 'd');
+  var space = game.board.space(4,1);
+  expect(space.piece).toBeNull();
+  space = game.board.space(8,5);
+  expect(space.piece).toBeInstanceOf(Piece);
+  expect(space.piece.type).toEqual('d');
+});
+
+test('removes a piece', () => {
+  var game = new_game();
+  expect(game.trolls.length).toEqual(8);
+  expect(game.indexed_trolls.length).toEqual(8);
+  expect(game.indexed_trolls[7]).toBeInstanceOf(Piece);
+  expect(game.indexed_trolls[7].type).toEqual('t');
+
+  game.remove_piece(game.board.space(8,8));
+  var space = game.board.space(8,8);
+  expect(space.piece).toBeNull();
+  expect(game.trolls.length).toEqual(7);
+  expect(game.indexed_trolls.length).toEqual(8);
+  expect(game.indexed_trolls[7]).toBeNull();
+});
+
+test('ends the game after 200 turns', () => {
+  var game = new_game();
+  game.end_turn();
+  expect(game.check_ending_conditions()).toBeUndefined();
+
+  game.turn_number = 201;
+  game.end_turn();
+  expect(game.check_ending_conditions()).toEqual({"reason": "Timeout"});
+})
+
+test('endsthe game if there are no more dwarves', () => {
+  var game = new_game();
+  game.end_turn();
+  expect(game.check_ending_conditions()).toBeUndefined();
+
+  game.dwarves = [];
+  game.end_turn();
+  expect(game.check_ending_conditions()).toEqual({"reason": "No more dwarves"});
+});
+
+test('endsthe game if there are no more trolls', () => {
+  var game = new_game();
+  game.end_turn();
+  expect(game.check_ending_conditions()).toBeUndefined();
+
+  game.trolls = [];
+  game.end_turn();
+  expect(game.check_ending_conditions()).toEqual({"reason": "No more trolls"});
+});
+
+test('ends the game if the players are finished', () => {
+  var game = new_game();
+  game.end_turn();
+  expect(game.check_ending_conditions()).toBeUndefined();
+
+  game.troll_controller.declared = true;
+  game.end_turn();
+  expect(game.check_ending_conditions()).toBeUndefined();
+
+  game.troll_controller.declared = false;
+  game.dwarf_controller.declared = true;
+  game.end_turn();
+  expect(game.check_ending_conditions()).toBeUndefined();
+
+  game.dwarf_controller.declared = true;
+  game.troll_controller.declared = true;
+  game.end_turn();
+  expect(game.check_ending_conditions()).toEqual({"reason": "Players agreed to finish"});
 });
