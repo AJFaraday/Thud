021c6600f0b22aa133020bfc9b82f0061fb5a6c5
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-05-27 14:40:38 +0100

    removed private fields, for now

--- src/lib/controller.js
+++ src/lib/controller.js
@@ -1,34 +1,32 @@
 const ControllerHelper = require('./controller_helper.js');
 
 class Controller {
-  #game
-  #helper
 
   constructor(game, side) {
-    this.#game = game;
+    this.game = game;
     this.side = side;
     this.current_space = null;
-    this.#helper = new ControllerHelper(this, game);
+    this.helper = new ControllerHelper(this, game);
   }
 
   turn() {
     var controller = this;
     return this.wrapper(() => {
-      return controller.#game.turn_number;
+      return controller.game.turn_number;
     });
   }
 
   scores() {
     var controller = this;
-    return this.wrapper(() => controller.#game.get_score());
+    return this.wrapper(() => controller.game.get_score());
   }
 
   spaces() {
     var controller = this;
     return this.wrapper(() => {
       return Array.from(
-        controller.#game.board.spaces,
-        space => controller.#helper.space_proxy(space)
+        controller.game.board.spaces,
+        space => controller.helper.space_proxy(space)
       );
     });
   }
@@ -36,7 +34,7 @@ class Controller {
   space_info(x, y) {
     var controller = this;
     return this.wrapper(() => {
-      return controller.#helper.space_info(x, y);
+      return controller.helper.space_info(x, y);
     });
   }
 
@@ -44,7 +42,7 @@ class Controller {
     var controller = this;
     return this.wrapper(() => {
         return Array.from(
-          controller.#game.dwarves,
+          controller.game.dwarves,
           dwarf => ({x: dwarf.x, y: dwarf.y})
         );
       }
@@ -55,7 +53,7 @@ class Controller {
     var controller = this;
     return this.wrapper(() => {
         return Array.from(
-          controller.#game.trolls,
+          controller.game.trolls,
           troll => ({x: troll.x, y: troll.y})
         );
       }
@@ -66,7 +64,7 @@ class Controller {
     var controller = this;
     return this.wrapper(() => {
         return Array.from(
-          controller.#game.indexed_dwarves,
+          controller.game.indexed_dwarves,
           dwarf => (dwarf ? {x: dwarf.x, y: dwarf.y} : null)
         );
       }
@@ -77,7 +75,7 @@ class Controller {
     var controller = this;
     return this.wrapper(() => {
         return Array.from(
-          controller.#game.indexed_trolls,
+          controller.game.indexed_trolls,
           troll => (troll ? {x: troll.x, y: troll.y} : null)
         );
       }
@@ -86,7 +84,7 @@ class Controller {
 
   previous_move() {
     var controller = this;
-    return this.wrapper(() => Object.assign(controller.#game.previous_move));
+    return this.wrapper(() => Object.assign(controller.game.previous_move));
   }
 
 // Actual actions
@@ -94,9 +92,9 @@ class Controller {
     var controller = this;
     return controller.wrapper(
       () => {
-        var space = controller.#game.board.space(x, y);
-        var moves = controller.#helper.moves_for(space);
-        controller.#game.report('highlight_space', controller.checked_space);
+        var space = controller.game.board.space(x, y);
+        var moves = controller.helper.moves_for(space);
+        controller.game.report('highlight_space', controller.checked_space);
         return moves;
       }
     );
@@ -106,10 +104,10 @@ class Controller {
     var controller = this;
     return controller.wrapper(
       () => {
-        var space = controller.#helper.space_proxy(controller.#game.board.space(x, y));
+        var space = controller.helper.space_proxy(controller.game.board.space(x, y));
         if (space) {
           if (!controller.checked_space || controller.checked_space.x != x || controller.checked_space.y != y) {
-            controller.#helper.moves_for(space);
+            controller.helper.moves_for(space);
           }
           if (space.piece && space.piece == controller.side) {
             controller.current_space = space;
@@ -124,9 +122,9 @@ class Controller {
   }
 
   check_move(x, y) {
-    var target_space = this.#game.board.space(x, y);
+    var target_space = this.game.board.space(x, y);
     if (this.current_space && target_space) {
-      var moves = this.#helper.moves_for(this.current_space);
+      var moves = this.helper.moves_for(this.current_space);
       var move = moves.find(move => move.x == x && move.y == y);
       if (move) {
         if (this.side == 'd') {
@@ -139,14 +137,14 @@ class Controller {
             kills = 0;
           }
           move.side = 'd';
-          this.#game.report('highlight_move', move);
+          this.game.report('highlight_move', move);
           return {valid: true, type: move.type, kills: kills, targets: targets};
         } else if (this.side == 't') {
           move.side = 't';
           move.targets = target_space.neighbours
             .filter(neighbour => neighbour.piece && neighbour.piece.type == 'd')
             .map(dwarf => ({x: dwarf.x, y: dwarf.y}));
-          this.#game.report('highlight_move', move);
+          this.game.report('highlight_move', move);
           return {valid: true, type: move.type, kills: move.targets.length, targets: move.targets};
         } else {
           return {valid: false, type: null, kills: 0, targets: []};
@@ -166,11 +164,11 @@ class Controller {
         );
         if (target) {
 
-          controller.#game.move_piece(target.x, target.y, target.type);
+          controller.game.move_piece(target.x, target.y, target.type);
           if (this.side == 't') {
-            controller.#helper.troll_swing(target.x, target.y);
+            controller.helper.troll_swing(target.x, target.y);
           }
-          controller.#game.previous_move = {
+          controller.game.previous_move = {
             from: {x: controller.current_space.x, y: controller.current_space.y},
             to: {x: target.x, y: target.y},
             side: controller.side,
@@ -178,7 +176,7 @@ class Controller {
             killed: target.kills
           }
           controller.clear_space();
-          controller.#game.end_turn();
+          controller.game.end_turn();
           return true;
         } else {
           controller.clear_space();
@@ -191,28 +189,28 @@ class Controller {
   clear_space() {
     this.current_space = null;
     this.checked_space = null;
-    this.#game.report('board_state');
+    this.game.report('board_state');
   }
 
   declare(game_over) {
     this.declared = game_over;
-    this.#game.report('player_declared', {side: this.side, game_over: game_over});
+    this.game.report('player_declared', {side: this.side, game_over: game_over});
     if(this.opponent_declared()) {
-      this.#game.end_turn();
+      this.game.end_turn();
     }
   }
 
   opponent_declared() {
     if (this.side == 'd') {
-      return this.#game.troll_controller.declared;
+      return this.game.troll_controller.declared;
     } else if (this.side == 't') {
-      return this.#game.dwarf_controller.declared;
+      return this.game.dwarf_controller.declared;
     }
   }
 
 
   wrapper(func) {
-    if (this.#game.current_side == this.side) {
+    if (this.game.current_side == this.side) {
       return func();
     } else {
       return null;
