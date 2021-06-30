4f332afc31c16acce7b48af998af1330f9d43892
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-06-30 16:52:38 +0100

    Automated deploy: 2021-06-30_16:52:38

--- grab_and_deploy.sh
+++ grab_and_deploy.sh
@@ -1,10 +1,14 @@
-node script/fetch_answers.js;
-node script/get_clients.js;
-node script/run_tournament.js;
+#! /bin/bash
+
+#node script/fetch_answers.js;
+#node script/get_clients.js;
+#node script/run_tournament.js;
 
 git add src/clients/*
 
-NOW=`date '+%F_%H:%M:%S'`;
-git commit -am 'Automated deploy: $NOW'
+now=`date '+%F_%H:%M:%S'`;
+commit_message="Automated deploy: ${now}"
+git commit -am "$commit_message"
+
 
 
